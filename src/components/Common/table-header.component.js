184deb713bb4721cc38a93ffc09cf9abf6d87315
Seefat Hossain Himel <seefathimel1@gmail.com>
2022-01-15 21:03:21 +0600

    class 18

--- src/components/Common/table-header.component.js
+++ src/components/Common/table-header.component.js
@@ -1,4 +1,4 @@
-const TableHeader = ({columns}) => {
+const TableHeader = ({ columns }) => {
     return (
         <thead>
             <tr>
--- src/components/movies.componnt.js
+++ src/components/movies.componnt.js
@@ -1,47 +1,22 @@
 import React, { Component } from "react";
 import Table from "./Common/table.component";
 import Rating from "./rating.component";
+import getMovies from "../service/get-movies.service";
 class Movies extends Component {
     state = {
         headers: ["Rank", "Titles", "IMDB Rating", "Your Rating"],
-        movies: [
-            {
-                rank: 1,
-                title: " The Shawshank Redemption (1994) ",
-                imdbRating: "9.2",
-                yourRating: false,
-            },
-            {
-                rank: 2,
-                title: " The Godfather (1972)",
-                imdbRating: "9.1",
-                yourRating: true,
-            },
-            {
-                rank: 3,
-                title: "The Godfather: Part II (1974)",
-                imdbRating: "9.0",
-                yourRating: false,
-            },
-            {
-                rank: 4,
-                title: "The Dark Knight (2008)",
-                imdbRating: "9.0",
-                yourRating: true,
-            },
-            {
-                rank: 5,
-                title: "12 Angry Men (1957)",
-                imdbRating: "8.9",
-                yourRating: false,
-            },
-        ],
+        movies: [],
     };
+
+    componentDidMount(){
+        const movies = getMovies();
+        this.setState({movies});
+    }
 
     handleIsRated = (movieRank) => {
         // console.log("rank ", movieRank );
         const movies = [...this.state.movies];
-        const movie = movies.find((movie) => movie.rank === movieRank);
+        const movie = movies.find((movie) => movie.id === movieRank);
         movie.yourRating = !movie.yourRating;
         this.setState({ movies });
     };
@@ -50,7 +25,7 @@ class Movies extends Component {
         const columns = [
             {
                 label: "Rank",
-                path: "rank",
+                path: "id",
                 content: (movie, key) => <td key={movie[key]}>{movie[key]}</td>,
             },
             {
@@ -59,9 +34,9 @@ class Movies extends Component {
                 content: (movie, key) => <td key={movie[key]}>{movie[key]}</td>,
             },
             {
-                label: "IMDB Rating",
-                path: "imdbRating",
-                content: (movie, key) => <td key={movie[key]}>{movie[key]}</td>,
+                label: "Poster",
+                path: "posterUrl",
+                content: (movie, key) => <td key={movie[key]}><img src={movie[key]} style={{height : "100px" , width : "auto"}} alt="" /></td>,
             },
             {
                 label: "Your Rating",
@@ -71,10 +46,15 @@ class Movies extends Component {
                         <Rating
                             isRated={movie[key]}
                             handleIsRated={this.handleIsRated}
-                            rank={movie.rank}
+                            rank={movie.id}
                         />
                     </td>
                 ),
+            },
+            {
+                label: "Action",
+                path: "action",
+                content: (movie, key) => <td key={movie[key]}>{movie[key]}</td>,
             },
         ];
         return (
